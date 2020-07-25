5af4f366f41a2060745d615a2da96d2a93534589
nicole <shchon@lunasoft.co.kr>
2020-07-25 10:16:28 +0900

    disable left distancing

--- Distance.py
+++ Distance.py
@@ -5,42 +5,42 @@ from typing import Tuple
 GPIO.setmode(GPIO.BCM)
  
 #You can use whichever GPIO pins you want
-LEFT_GPIO_TRIGGER = 18
-LEFT_GPIO_ECHO = 24
+# LEFT_GPIO_TRIGGER = 18
+# LEFT_GPIO_ECHO = 24
 
 RIGHT_GPIO_TRIGGER = 25
 RIGHT_GPIO_ECHO = 23
 
-GPIO.setup(LEFT_GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(LEFT_GPIO_ECHO, GPIO.IN)
+# GPIO.setup(LEFT_GPIO_TRIGGER, GPIO.OUT)
+# GPIO.setup(LEFT_GPIO_ECHO, GPIO.IN)
 
 GPIO.setup(RIGHT_GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(RIGHT_GPIO_ECHO, GPIO.IN)
 
 def distance() -> Tuple[float, float]:
-    print("LEFT SETUP")
-    GPIO.output(LEFT_GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(LEFT_GPIO_TRIGGER, False)
+    # print("LEFT SETUP")
+    # GPIO.output(LEFT_GPIO_TRIGGER, True)
+    # time.sleep(0.00001)
+    # GPIO.output(LEFT_GPIO_TRIGGER, False)
     
     print("RIGHT SETUP")
     GPIO.output(RIGHT_GPIO_TRIGGER, True)
     time.sleep(0.00001)
     GPIO.output(RIGHT_GPIO_TRIGGER, False)
  
-    left_start_time = time.time()
-    left_stop_time = time.time()
+    # left_start_time = time.time()
+    # left_stop_time = time.time()
 
     right_start_time = time.time()
     right_stop_time = time.time()
 
 
-    print("LEFT GPIO WHILE 1")
-    while GPIO.input(LEFT_GPIO_ECHO) == 0:
-        left_start_time = time.time()
-    print("LEFT GPIO WHILE 2")
-    while GPIO.input(LEFT_GPIO_ECHO) == 1:
-        left_stop_time = time.time()
+    # print("LEFT GPIO WHILE 1")
+    # while GPIO.input(LEFT_GPIO_ECHO) == 0:
+    #     left_start_time = time.time()
+    # print("LEFT GPIO WHILE 2")
+    # while GPIO.input(LEFT_GPIO_ECHO) == 1:
+    #     left_stop_time = time.time()
 
     print("RIGHT GPIO WHILE 1")
     while GPIO.input(RIGHT_GPIO_ECHO) == 0:
@@ -49,13 +49,14 @@ def distance() -> Tuple[float, float]:
     while GPIO.input(RIGHT_GPIO_ECHO) == 1:
         right_stop_time = time.time()
  
-    left_time_elapsed = left_stop_time - left_start_time
-    left_distance = (left_time_elapsed * 34300) / 2
+    # left_time_elapsed = left_stop_time - left_start_time
+    # left_distance = (left_time_elapsed * 34300) / 2
 
     right_time_elapsed = right_stop_time - right_start_time
     right_distance = (right_time_elapsed * 34300) / 2
  
-    return (left_distance, right_distance)
+    # return (left_distance, right_distance)
+    return (0, right_distance)
  
 if __name__ == '__main__':
     try:
