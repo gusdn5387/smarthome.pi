45613bf1a8b8a2928e01ecce92b9bc529e3b6afe
nicole <shchon@lunasoft.co.kr>
2020-07-31 21:31:48 +0900

    modify

--- src/light_control/server.py
+++ src/light_control/server.py
@@ -30,9 +30,12 @@ def update_light_onoff():
     """
     방 안에 있는 사람의 수를 구한 뒤 방의 전등 상태를 업데이트함
     """
-    if personcounter.get_room_person_count() <= 0:
+    perosn_count = personcounter.get_room_person_count()
+    if perosn_count < 0:
+        print("led off")
         led.off()
     else:
+        print("led on")
         led.on()
     
     return 'ok'
