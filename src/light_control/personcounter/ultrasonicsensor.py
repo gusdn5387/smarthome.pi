2b17f27a6048b2aac649dc6dbf9b8cf3de7aac38
nicole <shchon@lunasoft.co.kr>
2020-08-01 09:55:13 +0900

    logic modify

--- src/light_control/personcounter/ultrasonicsensor.py
+++ src/light_control/personcounter/ultrasonicsensor.py
@@ -63,7 +63,7 @@ class CloseDetected:
         만약 10초 전 close_detected가 true로 업데이트되었다면 close_detected를 업데이트하지 않음.
         """
         since_close_detected_updated_to_true = datetime.datetime.now() - self.detected_at
-        if since_close_detected_updated_to_true.total_seconds() < 10 and self.close_detected != close_detected:
+        if since_close_detected_updated_to_true.total_seconds() < 10 or self.close_detected == close_detected:
             return False
         else:
             self.detected_at = datetime.datetime.now()
